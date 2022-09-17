c78e1acdcab27e9cf5e8a5e9d1ad3e69ad6272c5
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2022-09-17 04:00:42 -0400

    Implement API

--- index.ts
+++ index.ts
@@ -3,14 +3,13 @@ import { nanoid } from "nanoid";
 
 const io = new Server(3000, { /* options */ });
 
-
+let id_map: any = {};
 let rooms: any = {};
 
 io.on("connection", (socket) => {
     console.log("a user connected: " + socket.handshake.address);
 
     socket.on('create_room', (data, callback) => {
-        nanoid()
         console.log(`create_room ${data.code}`);
         // check if room already exist
         if(rooms[data.code]) {
@@ -18,40 +17,46 @@ io.on("connection", (socket) => {
         }
         // actually make the room
         rooms[data.code] = { 'users': [] }
+        id_map[data.code] = {}
         callback('ok');
     });
 
     socket.on('join_room', (data, callback) => {
-        console.log(`join_room ${data.username} ${data.code}`);
+        console.log(`join_room ${data.code} ${data.session_id}`);
         // check room exists
         if(!rooms[data.code]) {
             callback('error');
         }
         // check if user already exists
-        if(rooms[data.code].users.includes(data.username)) {
+        if(!id_map[data.code].includes(data.session_id)) {
             callback('error');
         }
-        // insert user into room
-        rooms[data.code].users.push(data.username);
-        socket.to(data.code).emit('user_joined', { username: data.username });
         callback('ok');
     });
 
+    socket.on('join_new_room', (data, callback) => {
+        console.log(`join_new_room ${data.code} ${data.username}`);
+        // check room exists
+        if(!rooms[data.code]) {
+            callback('error');
+        }
+        let id = nanoid();
+        id_map[data.code][id] = data.username;
+        callback(id);
+        socket.to(data.code).emit('user_joined', { username: data.username, code: data.code });
+    });
+
     socket.on('send_message', (data, callback) => {
-        console.log(`send_message ${data.username} ${data.code} ${data.msg}`);
+        console.log(`send_message ${data.session_id} ${data.code} ${data.msg}`);
         // check room exists
         if(!rooms[data.code]) {
             callback('error');
         }
         // check user in room
-        if(!rooms[data.code].includes(data.username)) {
+        if(!id_map[data.code].includes(data.session_id)) {
             callback('error');
         }
-        socket.to(data.code).emit('message_sent', data);
+        socket.to(data.code).emit('message_sent', { username: id_map[data.code][data.session_id], code: data.code, msg: data.msg });
         callback('ok');
-    });
-
-    socket.on('disconnect', () => {
-        console.log('disconnect');  
     });
 });
