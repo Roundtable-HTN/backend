e40dd3feef3cd2aa385f875d4901a5ab3e98da61
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2022-09-17 07:21:10 -0400

    API changes

--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ import { nanoid } from "nanoid";
 
 const io = new Server(3000, { /* options */ });
 
-let id_map: any = {};
+let id_map: any = {}; // Map<Room Code, Map<Session ID, Username>>
 let rooms: any = {};
+
+let plugins: any = [];
+let instances: any = [];
+let lazy: any = [];
 
 io.on("connection", (socket) => {
     console.log("a user connected: " + socket.handshake.address);
@@ -18,6 +22,7 @@ io.on("connection", (socket) => {
         // actually make the room
         rooms[data.code] = { 'users': [] }
         id_map[data.code] = {}
+        socket.join(`room:${data.code}`)
         callback('ok');
     });
 
@@ -31,6 +36,7 @@ io.on("connection", (socket) => {
         if(!id_map[data.code].includes(data.session_id)) {
             callback('error');
         }
+        socket.join(`room:${data.code}`)
         callback('ok');
     });
 
@@ -43,7 +49,8 @@ io.on("connection", (socket) => {
         let id = nanoid();
         id_map[data.code][id] = data.username;
         callback(id);
-        socket.to(data.code).emit('user_joined', { username: data.username, code: data.code });
+        socket.join(`room:${data.code}`)
+        socket.to(`room:${data.code}`).emit('user_joined', { username: data.username, code: data.code });
     });
 
     socket.on('send_message', (data, callback) => {
@@ -56,7 +63,46 @@ io.on("connection", (socket) => {
         if(!id_map[data.code].includes(data.session_id)) {
             callback('error');
         }
-        socket.to(data.code).emit('message_sent', { username: id_map[data.code][data.session_id], code: data.code, msg: data.msg });
+        socket.to(`room:${data.code}`).emit('message_sent', { username: id_map[data.code][data.session_id], code: data.code, msg: data.msg });
         callback('ok');
+    });
+
+    socket.on("create_plugin", (data, callback) => {
+        console.log(`create_plugin ${data.code} ${data.plugin}`);
+        if(!rooms[data.code]) {
+            callback('error');
+        }
+        // todo: implement plugin creation
+        // allocate the plugin
+        if(lazy.length > 0) {
+            instances[lazy[lazy.length - 1]].push([]); // todo implement
+            lazy.pop(lazy.length - 1);
+        } else {
+            instances.push([]); // todo implement
+        }
+        callback('ok');
+    });
+
+    socket.on("join_plugin", (data, callback) => {
+        console.log(`join_plugin ${data.username} ${data.code} ${data.slot}`);
+        // check user in room
+        if(!id_map[data.code].includes(data.session_id)) {
+            callback('error');
+        }
+        // check room exists
+        if(!rooms[data.code]) {
+            callback('error');
+        }
+        // check plugin exists
+        if(0 > data.slot || data.slot > instances.length || instances[data.slot] === null) {
+            callback('error');
+        }
+        // allocate a room
+
+        const plugin_room = `plugin:${data.slot}`;
+        socket.join(plugin_room);
+        callback(plugin_room);
+  
+        socket.broadcast.to(plugin_room).emit("plugin_user_joined", {});
     });
 });
