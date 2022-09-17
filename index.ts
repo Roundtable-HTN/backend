5d32dd2477ab93002317f78aa96568a906f9f6da
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2022-09-17 03:36:22 -0400

    Continue implementing API

--- index.ts
+++ index.ts
@@ -1,30 +1,57 @@
 import { Server } from "socket.io";
+import { nanoid } from "nanoid";
 
 const io = new Server(3000, { /* options */ });
 
+
+let rooms: any = {};
+
 io.on("connection", (socket) => {
-    console.log("a user connected: " + socket.request.connection.remoteAddress);
+    console.log("a user connected: " + socket.handshake.address);
 
     socket.on('create_room', (data, callback) => {
+        nanoid()
         console.log(`create_room ${data.code}`);
-        // todo: check if room already exist
-        // todo: actually make the room
-        callback(true);
+        // check if room already exist
+        if(rooms[data.code]) {
+            callback('error');
+        }
+        // actually make the room
+        rooms[data.code] = { 'users': [] }
+        callback('ok');
     });
 
     socket.on('join_room', (data, callback) => {
         console.log(`join_room ${data.username} ${data.code}`);
-        // todo: check room exists
-        // todo: insert user into room
+        // check room exists
+        if(!rooms[data.code]) {
+            callback('error');
+        }
+        // check if user already exists
+        if(rooms[data.code].users.includes(data.username)) {
+            callback('error');
+        }
+        // insert user into room
+        rooms[data.code].users.push(data.username);
         socket.to(data.code).emit('user_joined', { username: data.username });
-        callback(true);
+        callback('ok');
     });
 
     socket.on('send_message', (data, callback) => {
         console.log(`send_message ${data.username} ${data.code} ${data.msg}`);
-        // todo: check user in room
-        // todo: check room exists
+        // check room exists
+        if(!rooms[data.code]) {
+            callback('error');
+        }
+        // check user in room
+        if(!rooms[data.code].includes(data.username)) {
+            callback('error');
+        }
         socket.to(data.code).emit('message_sent', data);
-        callback(true);
-    })
+        callback('ok');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('disconnect');  
+    });
 });
